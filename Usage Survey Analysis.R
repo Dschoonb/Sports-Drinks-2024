66d6dc9604616ef9755d437552dde3d2a83e8a6d
Dschoonb <78940595+Dschoonb@users.noreply.github.com>
2024-04-22 13:49:47 -0700

    1

--- Usage Survey Analysis.R
+++ Usage Survey Analysis.R
@@ -7,31 +7,31 @@ library(cluster)
 # All Users ---------------------------------------------------------------
 All<- read_excel("Usage Survey Results.xlsx", 
                                    sheet = "All")
-
-
-All$Cluster
+#Distance Matrix
 All.clust <- as.matrix(All)
 All.clust.d <- dist(All.clust, method = "binary")
+
+#Hierarchical Cluster Analysis
 All.clust.cd <- hclust(All.clust.d, method="ward.D2")
 plot(All.clust.cd, cex = 0.8, hang= -1)
-All.groups <- cutree(All.clust.cd, k=3)
 rect.hclust(All.clust.cd, k=3, border="red")
 
-All.groups <- as.data.frame(All.groups)
+  #Clusters to Column
 All.df <- as.data.frame(All)
-row.names(All.df) <- All.df$`Random ID`
-All.df$`Random ID` <- NULL 
-All.df$Cluster <- All.groups$All.groups
+All.groups <- cutree(All.clust.cd, k=3)
+All.df$Cluster <- All.groups
 
-EBar.pca <- PCA(All.df, axes = c(1,2))
+  #Perform factor analysis
+All.clust.means <- aggregate(as.matrix(All.df[,-1]) ~ Cluster, All.df, FUN = mean) #means for each cluster
+row.names(All.clust.means) <- All.clust.means$Cluster
+All.clust.means$Cluster <- NULL
 
+All.pca <- PCA(All.clust.means, axes = c(1,2))
+fviz_pca_var(All.pca, col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
+
+#Kmeans
 kfit <- kmeans(All.clust.d, 3)
 clusplot(as.matrix(All.clust.d), kfit$cluster, color=T, shade=T, labels=2, lines=0)
-
-All.df$poop <- kfit$cluster
-  kfit <- as.data.frame(kfit$cluster)
-write.csv(data, file="ClusteredRespondents.csv")
-
 
 
 # Exercise Users ---------------------------------------------------------------
